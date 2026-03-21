e10c19df35ae0fcde0821e5083982fe5358a1ff6
nisha272 <nisharana8077@gmail.com>
2026-03-21 18:35:25 +0530

    Add files via upload

---
+++ break.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int i=2;
+	switch(i)
+	{
+		case1:
+		printf("case1\n", );
+		break;
+		case2:
+		printf("case2\n", );
+		break;
+		case3:
+		printf("case3\n", );
+		break;
+	default:
+		printf("default\n");
+
+	}
+	return 0;
+}
