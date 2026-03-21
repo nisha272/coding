7020cbc2c33b576254ed4318b251de151abf08c8
nisha272 <nisharana8077@gmail.com>
2026-03-21 18:27:44 +0530

    Add files via upload

---
+++ case.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int i=2;
+	switch(i)
+	{
+		case1:
+		printf("i am case1\n" );
+		case2:
+		printf("i am case2\n");
+		case3:
+		printf("i am case3\n");
+	default:
+		printf("i am in default\n");
+
+	}
+	return 0;
+}
