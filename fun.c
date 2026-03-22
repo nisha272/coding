af8ed651e74af92027eef0ff27680e38b7f60c2d
nisha272 <nisharana8077@gmail.com>
2026-03-22 19:29:55 +0530

    Add files via upload

---
+++ fun.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void message();
+int main()
+{
+	message();
+	printf("cry and you stop the monotony\n" );
+	return 0;
+}
+void message()
+{
+	printf("smile and the world smiles with you\n");
+	
+}
