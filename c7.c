71d4c1b765412ecc6a82762b0c465858e237044e
nisha272 <nisharana8077@gmail.com>
2026-04-18 23:30:08 +0530

    Add files via upload

---
+++ c7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+
+    // Arithmetic operations to swap values
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("a = %d, b = %d\n", a, b);
+    return 0;
+}
