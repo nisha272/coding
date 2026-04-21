3c7a2ae357bfd8819479313d53eb46610c491e1a
nisha272 <nisharana8077@gmail.com>
2026-04-21 22:43:07 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void fun(int n){
+ for(int i=0;i<n;i++){
+	for(int j=0;j<n;j++){
+	cout<<"*";
+	}
+	cout<<endl;
+}}
+int main(){
+	int n=5;
+	fun(n);
+	return 0;
+}
