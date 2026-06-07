a3a89bf7369179b03975fe55f8b0b8c9aa7663d9
nisha272 <nisharana8077@gmail.com>
2026-06-07 13:54:02 +0530

    Add files via upload

---
+++ c48.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+unsigned int factorial(unsigned int n) {
+  
+    // Base Case:
+    if (n == 1) {
+        return 1;
+    }
+
+    // Multiplying the current N with the previous product
+    // of Ns
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d", num, factorial(num));
+    return 0;
+}
