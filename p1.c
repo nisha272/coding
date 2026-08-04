3876fed64ba96f6f0e9fb2a6ec4b339fc90bdd26
nisha272 <nisharana8077@gmail.com>
2026-08-04 19:56:54 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b;
+	
+	printf("no.1");
+	scanf("%d",&a);
+
+	printf("no.2");
+	scanf("%d",&b);
+
+	if(a>b){
+	printf("a greater");}
+	
+	else
+	printf("b greater");
+	
+}
